d75ed2cfad6ee26985525c873c7a97bd48de235b
stucksam <stucksam@students.zhaw.ch>
2024-12-01 11:48:48 +0100

    feat: do copying of dialect h5s concurrently

---
+++ TTS/TTS_CH/copy_h5_concurrently.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+from multiprocessing import Process
+
+LANG_MAP = {
+    'ch_be': 'Bern',
+    'ch_bs': 'Basel',
+    'ch_gr': 'Graubünden',
+    'ch_in': 'Innerschweiz',
+    'ch_os': 'Ostschweiz',
+    'ch_vs': 'Wallis',
+    'ch_zh': 'Zürich',
+    "de": "Deutschland"
+}
+LANG_MAP_INV = {v: k for k, v in LANG_MAP.items()}
+DATASETS_PATH = "/scratch/dialects"
+
+def copy_dialect(dialect: str):
+    print(f"Copying {dialect} to /scratch partition.")
+    shutil.copy2(os.path.join(DATASETS_PATH, f"{dialect}.hdf5"), DATASETS_PATH)
+    shutil.copy2(os.path.join(DATASETS_PATH, f"{dialect}.txt"), DATASETS_PATH)
+    print(f"Finished copying {dialect}.")
+
+
+def copy_dialects_to_cluster_concurrently():
+    if not os.path.exists(DATASETS_PATH):
+        os.makedirs(DATASETS_PATH)
+
+    processes = [
+        Process(target=copy_dialect, args=(dialect,))
+        for dialect, _ in LANG_MAP_INV.items()
+    ]
+
+    for process in processes:
+        process.start()
+
+    for process in processes:
+        process.join()
+
+
+if __name__ == '__main__':
+    copy_dialects_to_cluster_concurrently()
