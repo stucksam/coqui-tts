77d9398f21beb73ac48a879745b44b8568fc350d
stucksam <stucksam@students.zhaw.ch>
2024-12-16 10:40:20 +0100

    feat: add option to tar dialects on scratch partition

--- TTS/TTS_CH/copy_h5_concurrently.py
+++ TTS/TTS_CH/copy_h5_concurrently.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import tarfile
 from multiprocessing import Process
 
 LANG_MAP = {
@@ -17,19 +18,30 @@ DATASETS_PATH = "/cluster/home/stucksam/datasets/dialects"
 DIALECT_TRAIN_PATH = "/scratch/dialects"
 
 
-def copy_dialect(dialect: str):
+def copy_dialect(dialect: str, tar_files: bool = False) -> None:
     print(f"Copying {dialect} to /scratch partition.")
     shutil.copy2(os.path.join(DATASETS_PATH, f"{dialect}.hdf5"), DIALECT_TRAIN_PATH)
     shutil.copy2(os.path.join(DATASETS_PATH, f"{dialect}.txt"), DIALECT_TRAIN_PATH)
+
+    if tar_files:
+        dialect_files = [
+            os.path.join(DIALECT_TRAIN_PATH, f"{dialect}.hdf5"),
+            os.path.join(DIALECT_TRAIN_PATH, f"{dialect}.txt")
+        ]
+        with tarfile.open(os.path.join(DIALECT_TRAIN_PATH, f"{dialect}.tar.gzip"), "w:gz") as tar:
+            for file in dialect_files:
+                tar.add(file, arcname=file)
+        shutil.copy2(os.path.join(DIALECT_TRAIN_PATH, f"{dialect}.tar.gzip"), DATASETS_PATH)
+
     print(f"Finished copying {dialect}.")
 
 
-def copy_dialects_to_cluster_concurrently():
+def copy_dialects_to_cluster_concurrently() -> None:
     if not os.path.exists(DIALECT_TRAIN_PATH):
         os.makedirs(DIALECT_TRAIN_PATH)
 
     processes = [
-        Process(target=copy_dialect, args=(dialect,))
+        Process(target=copy_dialect, args=(dialect, True))
         for dialect in LANG_MAP_INV.keys()
     ]
 
