fix: rotation training params

TTS/tts/layers/xtts/trainer/dataset.py
@@ -76,8 +76,8 @@ class XTTSDataset(torch.utils.data.Dataset):
             self.h5_paths = {}
             for dataset_config in config.datasets:
                 # self.h5_files[dataset_config.dataset_name] = h5py.File(os.path.join(dataset_config.path, 'audio.h5'), 'r')
-                dialect_name = dataset_config.dataset_name
-                self.h5_paths[dialect_name] = os.path.join(dataset_config.path, f"{dialect_name}_{H5_SAMPLE_RATE}.hdf5")
+                dataset_name = dataset_config.dataset_name
+                self.h5_paths[dataset_name] = os.path.join(dataset_config.path, f"{dataset_name}.hdf5")
 
         assert self.max_wav_len is not None and self.max_text_len is not None
 