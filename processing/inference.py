eded71cbdeed8adee0fd19b4a8d1fbd0c25632e4
stucksam <stucksam@students.zhaw.ch>
2025-05-30 11:10:22 +0200

    feat: implement parallel inference

--- processing/inference.py
+++ processing/inference.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+import tarfile
+from multiprocessing import Process
 
 import librosa
 import pandas as pd
@@ -58,9 +60,23 @@ BATCH_SIZE = 32
 
 
 def setup_gpu_device() -> tuple:
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-    return device, torch_dtype
+    train_device = "cuda" if torch.cuda.is_available() else "cpu"
+    dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+    return train_device, dtype
+
+
+def collect_speaker_condition_samples():
+    wavs = {}
+    for dialect in LANG_MAP.keys():
+        if dialect == "de":
+            continue
+        ref_path = os.path.join(SPEAKER_DIRECTORY, dialect, "references")
+        refs = os.listdir(ref_path)
+
+        for speaker in refs:
+            wav_files = os.listdir(os.path.join(ref_path, speaker))
+            wavs[speaker] = [os.path.join(ref_path, speaker, wav) for wav in wav_files]
+    return wavs
 
 
 def assert_checkpoint_folder_contains_model(checkpoint_dir: str) -> None:
@@ -83,16 +99,83 @@ def assert_checkpoint_folder_contains_model(checkpoint_dir: str) -> None:
                 longest_trained_model = model
                 highest_step_count = steps
 
-        print(
-            f"Longest trained model at {highest_step_count} is {longest_trained_model} -> will be used for inference.")
+        print(f"Longest trained model at {highest_step_count} is {longest_trained_model} "
+              f"-> will be used for inference.")
         shutil.copyfile(os.path.join(checkpoint_dir, longest_trained_model), os.path.join(checkpoint_dir, "model.pth"))
 
 
-def run_inference(directory: str) -> None:
-    def save_list_to_df(entries, path: str) -> None:
-        df = pd.DataFrame(entries)
-        df.to_csv(path, index=False, encoding="utf-8", sep=";")
+def save_to_csv(entries: list | pd.DataFrame, path: str) -> None:
+    if isinstance(entries, list):
+        entries = pd.DataFrame(entries)
 
+    entries.to_csv(path, index=False, encoding="utf-8", sep=";")
+
+
+def combine_all_speaker_metadata(generated_speech_path: str) -> str:
+    combined_csv_path = os.path.join(generated_speech_path, "all_metadata.csv")
+    all_dfs = []
+    for root, dirs, files in os.walk(generated_speech_path):
+        if "metadata.csv" in files:
+            csv_path = os.path.join(root, "metadata.csv")
+            df = pd.read_csv(csv_path)
+            all_dfs.append(df)
+
+    # Combine all DataFrames
+    combined_df = pd.concat(all_dfs, ignore_index=True)
+
+    # Save the combined CSV
+    save_to_csv(combined_df, combined_csv_path)
+    return path
+
+
+def combine_dialect_metadata(generated_speech_path: str) -> None:
+    for speaker, wav in speaker_wavs.items():
+        out_wav_path = os.path.join(generated_speech_path, speaker)
+        meta_data_paths = [os.path.join(out_wav_path, file)
+                           for file in os.listdir(out_wav_path)
+                           if file.endswith(".csv")]
+        combined_df = pd.concat([pd.read_csv(path) for path in meta_data_paths], ignore_index=True)
+
+        save_to_csv(combined_df, os.path.join(out_wav_path, "metadata.csv"))
+
+        for path in meta_data_paths:  # delete dialect split metadata
+            os.remove(path)
+
+
+def run_inference_for_dialect(model_path: str, config_path: str, dial_tag: str) -> None:
+    """
+    Aimed at being run in parallel for each dialect to reduce inference time from 28h -> 3h.
+    """
+    print(f"Loading model for dialect {dial_tag}...")
+    # Init TTS
+    tts = TTS(
+        model_path=model_path,
+        config_path=config_path,
+        progress_bar=True
+    ).to(device)
+
+    generated_speech_path = os.path.join(model_path, "generated_speech")
+
+    print(f"Starting inference in {dial_tag} for all speakers.")
+    for speaker, wav in speaker_wavs.items():
+
+        out_wav_path = os.path.join(generated_speech_path, speaker)
+
+        df_dialect = []
+        for tid, text in enumerate(texts):
+            file_path = os.path.join(out_wav_path, f"{tid}_{LANG_MAP[dial_tag]}")
+            tts.tts_to_file(text=text, speaker_wav=wav, language=dial_tag, split_sentences=True,
+                            file_path=file_path)
+
+            speaker_wav_path = os.path.join(speaker, f"{tid}_{LANG_MAP[dial_tag]}.wav")
+            entry = {"tid": tid, "text": text, "dialect": LANG_MAP[dial_tag], "speaker": speaker,
+                     "file_path": speaker_wav_path}
+            df_dialect.append(entry)
+
+        save_to_csv(df_dialect, os.path.join(out_wav_path, f"metadata_{dial_tag}.csv"))
+
+
+def run_inference(directory: str) -> None:
     folder_name = os.path.basename(os.path.normpath(directory))
     model_path = os.path.join(OUT_PATH, folder_name)
     os.makedirs(model_path, exist_ok=True)
@@ -106,47 +189,40 @@ def run_inference(directory: str) -> None:
     shutil.copyfile(os.path.join(directory, "config.json"), config_path)
     shutil.copyfile(os.path.join(directory, "vocab.json"), vocab_path)
 
-    print("Loading model...")
-    # Init TTS
-    tts = TTS(
-        model_path=model_path,
-        config_path=config_path,
-        progress_bar=True
-    ).to(device)
-
     generated_speech_path = os.path.join(model_path, "generated_speech")
 
-    print("Starting inference per speaker")
-    all_speaker_wavs = []
+    # Create outdirs before strting inference to reduce potential I/O issues
     for speaker, wav in speaker_wavs.items():
-
         out_wav_path = os.path.join(generated_speech_path, speaker)
         os.makedirs(out_wav_path, exist_ok=True)
 
-        df_speaker = []
-        for dial_tag in LANG_MAP.keys():
-            for tid, text in enumerate(texts):
-                file_path = os.path.join(out_wav_path, f"{tid}_{LANG_MAP[dial_tag]}")
-                tts.tts_to_file(text=text, speaker_wav=wav, language=dial_tag, split_sentences=True,
-                                file_path=file_path)
+    processes = [
+        Process(target=run_inference_for_dialect, args=(model_path, config_path, dial_tag,))
+        for dial_tag in LANG_MAP.keys()
+    ]
 
-                speaker_wav_path = os.path.join(speaker, f"{tid}_{LANG_MAP[dial_tag]}.wav")
-                entry = {"tid": tid, "text": text, "dialect": LANG_MAP[dial_tag], "speaker": speaker,
-                         "file_path": speaker_wav_path}
-                df_speaker.append(entry)
-                all_speaker_wavs.append(entry)
+    for process in processes:
+        process.start()
 
-        save_list_to_df(df_speaker, os.path.join(out_wav_path, f"{speaker}.csv"))
+    for process in processes:
+        process.join()
+
+    combine_dialect_metadata(generated_speech_path)
+    meta_data_path = combine_all_speaker_metadata(generated_speech_path)
 
     print("Saving texts to csv...")
     df_texts = []
     for tid, text in enumerate(texts):
         df_texts.append({"tid": tid, "text": text})
-    save_list_to_df(df_texts, os.path.join(generated_speech_path, "texts.csv"))
-    save_list_to_df(all_speaker_wavs, os.path.join(generated_speech_path, "wav_files.csv"))
+    save_to_csv(df_texts, os.path.join(generated_speech_path, "texts.csv"))
+
+    generated_speech_zip = os.path.join(model_path, "generated_speech.zip")
+    with tarfile.open(generated_speech_zip, "w:gz") as tar:
+        tar.add(generated_speech_path, arcname="generated_speech")
 
     print("Copying generated speech to projects folder...")
-    shutil.copytree(generated_speech_path, save_eval_path, dirs_exist_ok=True)
+    shutil.copytree(generated_speech_zip, save_eval_path, dirs_exist_ok=True)
+    shutil.copyfile(meta_data_path, os.path.join(save_eval_path, "metadata.csv"))
 
     # cleanup scratch
     print("Deleting generated speech from scratch folder...")
@@ -195,11 +271,6 @@ def _setup_phoneme_model() -> Pipeline:
     )
 
 
-# def _setup_whisperx_pipeline() -> FasterWhisperPipeline:
-#     model = whisperx.load_model("large-v3", device, language="de", compute_type="float16",
-#                                 download_root=MODEL_DOWNLOAD_PATH)
-
-
 def transcribe_audio_to_german_and_phoneme(model_path: str) -> None:
     df = load_wav_metadata(model_path)
     num_samples = len(df)
@@ -246,7 +317,6 @@ def transcribe_audio_to_german_and_phoneme(model_path: str) -> None:
         "Detected missmatch between generated number of phoneme texts and number of samples"
     df["phoneme"] = phoneme_text
 
-
     df.to_csv(os.path.join(model_path, "generated_speech", "transcribed_metadata.csv"), sep=";", index=False,
               encoding="utf-8")
 
@@ -258,17 +328,7 @@ if __name__ == "__main__":
     device, torch_dtype = setup_gpu_device()
 
     texts = [f"Das ist ein Beispielsatz, welcher auf Schweizerdeutsch ausgesprochen werden soll"]
-    speaker_wavs = {}
-    for dial_tag in LANG_MAP.keys():
-        if dial_tag == "de":
-            continue
-        ref_path = os.path.join(SPEAKER_DIRECTORY, dial_tag, "references")
-        refs = os.listdir(ref_path)
-
-        for speaker in refs:
-            wav_files = os.listdir(os.path.join(ref_path, speaker))
-            speaker_wavs[speaker] = [os.path.join(ref_path, speaker, wav) for wav in wav_files]
-
+    speaker_wavs = collect_speaker_condition_samples()
     list_of_directories = [os.path.join(MODEL_CHECKPOINTS_PATH, model_dir) for model_dir in
                            os.listdir(MODEL_CHECKPOINTS_PATH) if "SwissGPC" in model_dir]
 
